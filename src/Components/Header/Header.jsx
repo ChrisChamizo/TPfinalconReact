db91e3019ec6482e74bccfeeb7a0fc600682bab1
ChrisChamizo <christianchamizo@live.com.ar>
2024-07-14 02:17:09 -0300

    Creo la funcion Header y la llamo en el main

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,2 +1,24 @@
 /*Importo el css del header*/
-import "./Header.css" 
+import "./Header.css"
+
+/*Creo la funcion Header*/
+function Header(){
+    return (
+        <header>
+        <h2>CC</h2>
+        <h1>Christian Chamizo</h1>
+        <div>
+            <button className="inicio">INICIO</button>
+            <button>SOBRE MI</button>
+            <button>SERVICIOS</button>
+            <button>RESUMEN</button>
+            <button>PORFOLIO</button>
+            <button>CONTACTAME</button>
+        </div>
+    </header>
+);
+}
+
+export {Header};
+    
+ 
