edbedda699f2c9b0760cbcff93f6ec9470b3fe7c
ChrisChamizo <christianchamizo@live.com.ar>
2024-07-24 16:49:10 -0300

    Se agrego los padding y margin a todos los componentes box,Ajuste del espaciado entre los recuadros

--- src/Components/Resumen/Resumen.jsx
+++ src/Components/Resumen/Resumen.jsx
@@ -4,49 +4,47 @@ import { Box, Heading, Text, Divider, Progress, Stack,extendTheme, ChakraProvide
 
 
 /* Función Resumen */
-function Resumen() {
-  return (
-    <Box as="section" p="5px" bg="gray.900" color="white" h="100vh">
-      <Heading as="h1" mb="3px" textAlign="center" size="lg">
-        RESUMEN
-      </Heading>
-      <Divider borderColor="customRed.500" borderWidth="2px" maxW="100px" mx="auto" mt="3" mb="5" />
+function Resumen(){
+  return(
+    <Box as="section" p="5" bg="gray.900" color="white" h="100vh">
+        <Heading as="h1" mb="3px" textAlign="center" size="lg">
+          RESUMEN
+        </Heading>
+        <Divider borderColor="customRed.500" borderWidth="2px" maxW="100px" mx="auto" mt="3" mb="5" />
 
-      <Stack spacing="8" maxW="800px" mx="auto">
-        <Box textAlign="left" bg="gray.800" p="5px" borderRadius="md">
+        <Stack spacing="8" maxW="750px" mx="auto">
+          <Box flexBasis="100%" textAlign="left" bg="gray.800" p="20px" borderRadius="md">
 
-          <Heading as="h2" size="md" mb="2">2018 - Actualidad</Heading>
-
-          <Divider my="3" borderColor="customRed.500" borderWidth="2px" maxW="100px" />
-          <Text fontWeight="bold">Tecnicatura Universitaria en Informática</Text>
-          <Text>Universidad Nacional De General Sarmiento (UNGS)</Text>
-        </Box>
-
-        <Stack direction="row" spacing="5" wrap="wrap" justify="center">
-
-          <Box flexBasis="45%" bg="gray.800" p="5px" borderRadius="md">
-
-            <Heading as="h2" size="sm" mb="2">DISEÑO</Heading>
-            <Progress value={80} size="sm" mb="4" colorScheme="customRed" />
-
-            <Heading as="h2" size="sm" mb="2">APP DE ESCRITORIO</Heading>
-            <Progress value={85} size="sm" mb="4" colorScheme="customRed" />
-
-            <Heading as="h2" size="sm" mb="2">MANTENIMIENTO WEB</Heading>
-            <Progress value={70} size="sm" mb="4" colorScheme="customRed" />
+            
+            <Heading as="h2" size="md" mb="2">2018 - Actualidad</Heading>
+            <Divider my="3" borderColor="customRed.500" borderWidth="2px" maxW="100px" />
+            <Text fontWeight="bold">Tecnicatura Universitaria en Informática</Text>
+            <Text>Universidad Nacional De General Sarmiento (UNGS)</Text>
           </Box>
 
-          <Box flexBasis="45%" bg="gray.800" p="5" borderRadius="md">
+          <Stack direction="row" spacing="4" wrap="wrap" justify="center">
+            <Box flexBasis={{ base: "100%", md: "45%" }} 
+            bg="gray.800" p="20px" borderRadius="md" textAlign="left">
 
-            <Heading as="h2" size="sm" mb="2">HTML</Heading>
-            <Progress value={70} size="sm" mb="4" colorScheme="customRed" />
+              <Heading as="h2" size="sm" mb="2">DISEÑO</Heading>
+              <Progress value={80} size="sm" mb="4" colorScheme="customRed" />
 
-            <Heading as="h2" size="sm" mb="2">CSS</Heading>
-            <Progress value={75} size="sm" mb="4" colorScheme="customRed" />
-          </Box>
+
+              <Heading as="h2" size="sm" mb="2  ">APP DE ESCRITORIO</Heading>
+              <Progress value={85} size="sm" mb="4" colorScheme="customRed" />
+              <Heading as="h2" size="sm" mb="2">MANTENIMIENTO WEB</Heading>
+              <Progress value={70} size="sm" mb="4" colorScheme="customRed" />
+            </Box>
+
+            <Box flexBasis={{ base: "100%", md: "45%" }} bg="gray.800" p="20px" borderRadius="md" textAlign="left">
+              <Heading as="h2" size="sm" mb="2">HTML</Heading>
+              <Progress value={70} size="sm" mb="4" colorScheme="customRed" />
+              <Heading as="h2" size="sm" mb="2">CSS</Heading>
+              <Progress value={75} size="sm" mb="4" colorScheme="customRed" />
+            </Box>
+          </Stack>
         </Stack>
-      </Stack>
-    </Box>
-  );
+      </Box>
+  )
 }
 export {Resumen}
