a0363ceae735c36e41121460991ca0086d338678
ChrisChamizo <christianchamizo@live.com.ar>
2024-07-27 21:57:39 -0300

    Se estilo el componente hero completo

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,55 +1,65 @@
+/*Importo los componentes de CHACKA IU*/ 
+import { Box, Flex, Heading, Image, Text, Link, Icon, Button, VStack } from "@chakra-ui/react";
+/*Importo los iconos de react-icons*/ 
+import { FaFacebook, FaInstagram, FaWhatsapp, FaTwitter, FaHandSparkles } from "react-icons/fa"
+/*Importo la foto personal para el hero*/ 
+import fotopersonal from "../../assets/img/fotopersonal.jpg";
 
-/*Importo las imagenes de la seccion hero*/
-import fotopersonal from "../../assets/img/fotopersonal.jpg"
+const Hero = () => {
+    return (
+      <Box as="main" bg="#0a0f1a" color="white" textAlign="center" p="4">
+        <Box as="section" className="hero" mb="8" h="100vh">
+          <Flex className="contenedor-contenido-imagen" align="center" justify="space-between">
 
 
-/*Creo la funcion hero*/
-const Hero = () =>{
-    return(
-        
-        <main>
-            <section className="hero">
-                <div className="contenedor-contenido-imagen">
-                    <div className="hero-content">
-                        <h2><i className="fa-solid fa-hand-sparkles"></i> HOLA!, SOY</h2>
-                        <div className="profesion">
-                            <h2>DISEÑADOR WEB</h2>
-                        </div>
-                        <div className="nombre">
-                            <h1>CHRISTIAN CHAMIZO</h1>
-                        </div>
-                    </div>
-                    <div className="fotopersonal">
-                        <img src ={fotopersonal} alt="Foto personal" />
-                    </div>
-                </div>
-            </section>
-            
-            <div className="parrafo-hero">
-                <p>Diseñador web, programador, estudiante de sistemas y más...</p>
-            </div>
-            
-            <section className="redes-botones">
-                <div className="iconos-redes">
-                    <a href="http://es-la.facebook.com"><i className="fa-brands fa-facebook"></i></a>
-                    <a href="https://www.instagram.com/chris.chamy_/"><i className="fa-brands fa-instagram"></i></a>
-                    <a href="http://wa.me/+541132262737"><i className="fa-brands fa-whatsapp"></i></a>
-                    <a href="http://x.com/?lang=ES"><i className="fa-brands fa-twitter"></i></a>
-                </div>
-                
-                <div className="contenedor-botones">
-                    <div className="boton-contactame">
-                        <button>Contáctame <span className="arrow">→</span></button>
-                    </div>
-                    
-                    <div className="boton-porfolio">
-                        <a href="https://github.com/ChrisChamizo"><button>Porfolio</button></a>
-                    </div>
-                </div>
-            </section>
-        </main>
-       
+            <Box className="hero-content" mb="4" textAlign="left" mr="4" ml="14">
+              <Heading as="h2" size="md" mb="2">
+                <Icon as={FaHandSparkles} mr="2" color="yellow.400" />
+                HOLA!, SOY
+              </Heading>
+              <Heading as="h2" size="2xl" fontWeight="bold" mb="2">
+                DISEÑADOR WEB
+              </Heading>
+              <Heading as="h1" size="2xl" color="#d0002b">
+                CHRISTIAN CHAMIZO
+              </Heading>
+              <Text fontSize="lg" mt="4">
+                Diseñador Web, Programador, Freelancer, Gamer Y Más...
+              </Text>
+  
+              <Flex mt="8" align="center">
+                <Flex className="iconos-redes" mb="1" mr="4">
+                  <Link href="http://es-la.facebook.com" isExternal mx="2">
+                    <Icon as={FaFacebook} boxSize="6" color="white" />
+                  </Link>
+                  <Link href="https://www.instagram.com/chris.chamy_/" isExternal mx="2">
+                    <Icon as={FaInstagram} boxSize="6" color="white" />
+                  </Link>
+                  <Link href="http://wa.me/+541132262737" isExternal mx="2">
+                    <Icon as={FaWhatsapp} boxSize="6" color="white" />
+                  </Link>
+                  <Link href="http://x.com/?lang=ES" isExternal mx="2">
+                    <Icon as={FaTwitter} boxSize="6" color="white" />
+                  </Link>
+                </Flex>
+  
+                <Flex className="contenedor-botones">
+                  <Button as="a" href="http://wa.me/+541132262737" colorScheme="whiteAlpha" variant="outline" mx="2">
+                    Contáctame <Box as="span" ml="2"> &rarr;</Box>
+                  </Button>
+                  <Button as="a" href="https://github.com/ChrisChamizo" colorScheme="whiteAlpha" variant="outline" mx="2">
+                    Portafolio
+                  </Button>
+                </Flex>
+              </Flex>
+            </Box>
+            <Box className="fotopersonal" mr="8">
+              <Image src={fotopersonal} alt="Foto personal" borderRadius="md" w="400px" h="450px" />
+            </Box>
+          </Flex>
+        </Box>
+      </Box>
     );
-};
-
-export {Hero}
+  };
+  
+  export { Hero };
