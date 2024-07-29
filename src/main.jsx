9a22336496ede22f18d12178f407e85e19c3472a
ChrisChamizo <christianchamizo@live.com.ar>
2024-07-28 23:14:04 -0300

    Se le cambio el nombre del archivo index.css a responsive.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './responsive.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import Theme from './Theme/Theme.jsx';
 
