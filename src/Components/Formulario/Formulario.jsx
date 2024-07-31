86fc60a1edd6b5429098eae57443ed3e0d22dc51
ChrisChamizo <christianchamizo@live.com.ar>
2024-07-31 00:41:02 -0300

    Se realizo el responsive al componente Formulario y se ajustaron varias popiedades para que se adapten a distintos tamaños de pantalla. Padding, espaciados, ancho, alto etc

--- src/Components/Formulario/Formulario.jsx
+++ src/Components/Formulario/Formulario.jsx
@@ -30,32 +30,76 @@ const Formulario = () => {
   }
 
   return (
-    <Box bg="#141b24" color="#fff" p="10px">
-      <Box textAlign="center">
-        <Heading as="h1" fontSize="32px" color="rgb(205,207,208)">
+    <Box 
+    bg="#141b24" 
+    color="#fff" 
+    p={{base:"4",md:"6",lg:"10"}}>
+      <Box 
+      textAlign="center"
+      >
+        <Heading 
+        as="h1" 
+        fontSize={{base:"24px",md:"28px",lg:"32px"}} 
+        color="rgb(205,207,208)"
+        >
           CONTACTO
         </Heading>
-        <Divider my="3" borderColor="customRed.500" borderWidth="2px" maxW="100px" mx="auto" mt="3" mb="5"/>
+        <Divider
+        my="3"
+        borderColor="customRed.500"
+        borderWidth="2px"
+        maxW="100px"
+        mx="auto"
+        mt="3"
+        mb="5"/>
         
       </Box>
 
-      <Flex justify="space-between" maxW="1200px" mx="auto">
-        <Box w="40%">
-          <VStack align="start" spacing={4} ml="8px">
-            <Flex align="center">
-              <Icon as={FaMapMarkerAlt} mr="2px" />
-              <Text>Ubicación: Buenos Aires, Argentina</Text>
+      <Flex
+      direction={{base:"column",md:"row"}}
+       justify="space-between"
+        maxW="1200px"
+        mx="auto"
+        spacing={{base:"4",md:"8"}}>
+        <Box
+         w={{base:"100%",md:"40%"}}
+         mb={{base:"4", md:"0"}}>
+         
+          <VStack
+            align="start"
+            spacing="4"
+            ml={{base:"0", md:"8px"}}>
+            <Flex
+             align="center">
+              <Icon
+               as={FaMapMarkerAlt} 
+               mr="2px" />
+              <Text>
+              Ubicación: Buenos Aires, Argentina
+              </Text>
             </Flex>
-            <Flex align="center">
-              <Icon as={FaEnvelope} mr="2px" />
-              <Text>Correo electrónico: Christianchamizo@live.com.ar</Text>
+            <Flex 
+            align="center">
+              <Icon
+              as={FaEnvelope} 
+              mr="2px" />
+              <Text>
+              Correo electrónico: Christianchamizo@live.com.ar
+              </Text>
             </Flex>
-            <Flex align="center">
-              <Icon as={FaPhone} mr="2px" />
-              <Text>Teléfono: +54 11-3226-2737</Text>
+            <Flex 
+            align="center">
+              <Icon 
+              as={FaPhone} 
+              mr="2px" />
+              <Text>
+                Teléfono: +54 11-3226-2737
+                </Text>
             </Flex>
           </VStack>
-          <Box mt="6px" ml="8px"> 
+          <Box 
+          mt="6px" 
+          ml={{base:"0",md:"8px"}}> 
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d105073.23949906467!2d-58.43329845000001!3d-34.615823750000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca3b4ef90cbd%3A0xa0b3812e88e88e87!2sBuenos%20Aires%2C%20Cdad.%20Aut%C3%B3noma%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1717097539226!5m2!1ses-419!2sar"
               width="400"
@@ -68,11 +112,23 @@ const Formulario = () => {
           </Box>
         </Box>
 
-        <Box w="50%">
-          <Box as="form" onSubmit={EnvioFormulario} mx="auto">
-            <VStack spacing={4}>
-              <Flex className="formulario-nombre-apellido" w="100%" justify="space-between" mb="15px" p="10px 20px">
-                <FormControl id="nombre" w="48%">
+        <Box
+         w={{base:"100%", md:"50%"}}>
+          <Box 
+          as="form" 
+          onSubmit={EnvioFormulario} 
+          mx="auto">
+            <VStack
+             spacing="4">
+              <Flex
+              direction={{base:"column",sm:"row"}}
+              w="100%"
+              justify="space-between"
+              mb="15px"
+              p="10px 20px">
+                <FormControl 
+                id="nombre" 
+                w={{base:"100%",sm:"48%"}}>
                   <Input
                     type="text"
                     placeholder="Nombre"
@@ -87,7 +143,10 @@ const Formulario = () => {
                     _focus={{ borderColor: '#007bff', outline: 'none' }}
                   />
                 </FormControl>
-                <FormControl id="apellido" w="48%">
+                <FormControl 
+                id="apellido"
+                w={{base:"100%", sm:"48%"}}
+                mt={{base:"4", sm:"0"}}>
                   <Input
                     type="text"
                     placeholder="Apellido"
@@ -102,8 +161,10 @@ const Formulario = () => {
                   />
                 </FormControl>
               </Flex>
-              <Box className="formulario-asunto" w="95%">
-                <FormControl id="asunto">
+              <Box 
+               w="95%">
+                <FormControl 
+                id="asunto">
                   <Input
                     type="text"
                     placeholder="Asunto"
@@ -119,7 +180,9 @@ const Formulario = () => {
                   />
                 </FormControl>
               </Box>
-              <FormControl id="mensaje" w="95%">
+              <FormControl 
+              id="mensaje"
+              w="95%">
                 <Textarea
                   name="mensaje"
                   value={datos.mensaje}
@@ -143,7 +206,11 @@ const Formulario = () => {
                 fontSize="16px"
                 cursor="pointer"
               >
-                Enviar Mensaje <Box as="span" ml="10px">➤</Box>
+                Enviar Mensaje <Box
+                 as="span"
+                 ml="10px">
+                  ➤
+                </Box>
               </Button>
             </VStack>
           </Box>
