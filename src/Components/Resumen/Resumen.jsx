76decb2c10e143cfa27fd4f301e1236a556367dd
ChrisChamizo <christianchamizo@live.com.ar>
2024-07-30 23:57:04 -0300

    Se aplico el responsive al componente Resumen

--- src/Components/Resumen/Resumen.jsx
+++ src/Components/Resumen/Resumen.jsx
@@ -6,41 +6,140 @@ import { Box, Heading, Text, Divider, Progress, Stack,extendTheme, ChakraProvide
 /* Función Resumen */
 function Resumen(){
   return(
-    <Box as="section" p="5" bg="gray.900" color="white" h="100vh">
-        <Heading as="h1" mb="3" textAlign="center" fontSize="32px">
+    <Box 
+    as="section" 
+    p={{base:"5",md:"10"}} 
+    bg="gray.900"
+    color="white"
+     h="100vh">
+        <Heading 
+        as="h1" 
+        mb="3" 
+        textAlign="center" 
+        fontSize={{base:"24px",md:"32px"}}>
           RESUMEN
         </Heading>
-        <Divider borderColor="customRed.500" borderWidth="2px" maxW="100px" mx="auto" mt="3" mb="5" />
+        <Divider 
+        borderColor="customRed.500" 
+        borderWidth="2px" 
+        maxW="100px"
+        mx="auto"
+        mt="3" 
+        mb="5" />
 
-        <Stack spacing="8" maxW="750px" mx="auto">
-          <Box flexBasis="100%" textAlign="left" bg="gray.800" p="20px" borderRadius="md">
+        <Stack 
+        spacing="8" 
+        maxW="750px"
+        mx="auto">
+          <Box 
+          flexBasis="100%" 
+          textAlign="left" 
+          bg="gray.800"
+          p={{base:"5",md:"20px"}} 
+          borderRadius="md">
 
             
-            <Heading as="h2" size="md" mb="2">2018 - Actualidad</Heading>
-            <Divider my="3" borderColor="customRed.500" borderWidth="2px" maxW="100px" />
+            <Heading 
+            as="h2" 
+            size="md" 
+            mb="2">
+            2018 - Actualidad
+            </Heading>
+            <Divider 
+            my="3" 
+            borderColor="customRed.500" 
+            borderWidth="2px" 
+            maxW="100px" />
             <Text fontWeight="bold">Tecnicatura Universitaria en Informática</Text>
-            <Text>Universidad Nacional De General Sarmiento (UNGS)</Text>
+            <Text>
+              Universidad Nacional De General Sarmiento (UNGS)
+              </Text>
           </Box>
 
-          <Stack direction="row" spacing="4" wrap="wrap" justify="center">
-            <Box flexBasis={{ base: "100%", md: "45%" }} 
-            bg="gray.800" p="20px" borderRadius="md" textAlign="left">
+          <Stack
+          direction={{base:"column",md:"row"}} 
+          spacing="4" 
+          wrap="wrap" 
+          justify="center">
+            <Box 
+            flexBasis={{ base: "100%", md: "45%" }} 
+            bg="gray.800" 
+            p={{base:"5",md:"20px"}}
+            borderRadius="md" 
+            textAlign="left">
 
-              <Heading as="h2" size="sm" mb="2">DISEÑO</Heading>
-              <Progress value={80} size="sm" mb="4" colorScheme="customRed" />
+              <Heading
+               as="h2"
+                size="sm"
+                 mb="2">
+                  DISEÑO
+                </Heading>
+              <Progress
+               value={80} 
+               size="sm"
+                mb="4" 
+                colorScheme="customRed" />
 
-
-              <Heading as="h2" size="sm" mb="2  ">APP DE ESCRITORIO</Heading>
-              <Progress value={85} size="sm" mb="4" colorScheme="customRed" />
-              <Heading as="h2" size="sm" mb="2">MANTENIMIENTO WEB</Heading>
-              <Progress value={70} size="sm" mb="4" colorScheme="customRed" />
+              <Heading
+               as="h2"
+                size="sm"
+                 mb="2"
+                 >
+                APP DE ESCRITORIO
+                </Heading>
+              <Progress
+               value={85}
+                size="sm"
+                mb="4"
+                colorScheme="customRed"
+                 />
+              <Heading
+               as="h2"
+              size="sm"
+              mb="2"
+              >
+              MANTENIMIENTO WEB
+            </Heading>
+              <Progress
+               value={70}
+              size="sm"
+              mb="4"
+              colorScheme="customRed" 
+              />
             </Box>
 
-            <Box flexBasis={{ base: "100%", md: "45%" }} bg="gray.800" p="20px" borderRadius="md" textAlign="left">
-              <Heading as="h2" size="sm" mb="2">HTML</Heading>
-              <Progress value={70} size="sm" mb="4" colorScheme="customRed" />
-              <Heading as="h2" size="sm" mb="2">CSS</Heading>
-              <Progress value={75} size="sm" mb="4" colorScheme="customRed" />
+            <Box 
+            flexBasis={{ base: "100%", md: "45%" }} 
+            bg="gray.800" 
+            p={{base:"5",md:"20px"}} 
+            borderRadius="md" 
+            textAlign="left">
+              <Heading
+              as="h2"
+              size="sm"
+              mb="2"
+               >
+              HTML
+              </Heading>
+              <Progress
+               value={70}
+              size="sm" 
+              mb="4" 
+              colorScheme="customRed"
+              />
+              <Heading
+              as="h2"
+              size="sm"
+              mb="2"
+              >
+              CSS
+              </Heading>
+              <Progress
+               value={75}
+              size="sm"
+              mb="4" 
+              colorScheme="customRed"
+              />
             </Box>
           </Stack>
         </Stack>
