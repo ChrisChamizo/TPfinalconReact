28f2123d3b51ba8361c59a0ada0050ba1ed340c8
ChrisChamizo <christianchamizo@live.com.ar>
2024-07-31 00:39:41 -0300

    Se realizo el responsive al componente MiPortafolio y se cambio el componente Hstack a Stack para soportar los elementos en distintos tamaños de pantalla. Se ajusto varias propiedades.

--- src/Components/Miportatfolio/Miportafolio.jsx
+++ src/Components/Miportatfolio/Miportafolio.jsx
@@ -8,52 +8,99 @@ import imagentpreact from '../../assets/img/imagentpreact.png';
 
 /* Importación de los componentes de Chakra UI */
 import { Box, Heading, Image, Link, Text, HStack,Divider } from '@chakra-ui/react';
-/*Utilizo el componente HStack para alinear horizontalmente las i*/ 
+/*Utilizo el componente HStack para alinear horizontalmente las imagenes*/ 
 
 /* Creación de la función MiPortfolio */
 const MiPortafolio = () => {
-    return (
-      <Box bg="#101324" color="white" py="10">
-        <Box textAlign="center" mb="3">
-          <Heading as="h1" fontSize="32px">
-            MI PORTAFOLIO
-          </Heading>
-          <Divider borderColor="customRed.500" borderWidth="2px" maxW="100px" mx="auto" mt="3" mb="5"/>
-        </Box>
-  
-        <Box mx="auto" maxW="1200px" px="5">
-          <HStack spacing="6" justify="center">
+  return (
+    <Box 
+    bg="#101324" 
+    color="white" 
+    py={{ base: "10", md: "20" }}>
+      <Box 
+      textAlign="center" 
+      mb="3"
+      >
+        <Heading
+         as="h1"
+         fontSize={{ base: "24px", md: "32px" }}>
+          MI PORTAFOLIO
+        </Heading>
+        <Divider
+         borderColor="customRed.500"
+         borderWidth="2px" 
+         maxW="100px" 
+         mx="auto" 
+         mt="3" 
+         mb="5" />
+      </Box>
 
-            {/* Contiene las capturas de pantalla */}
-            <Box className="screenshot-portfolio">
-              <Image src={imagentp3} alt="screenshot de porfolio3" borderRadius="md" boxShadow="lg" />
-            </Box>
-  
-            <Box className="screenshot-portfolio">
-              <Image src={imagentp4} alt="screenshot de porfolio4" borderRadius="md" boxShadow="lg" />
-            </Box>
-            <Box className="screenshot-portfolio">
-            <Image src={imagentpreact} alt="screenshot de porfolio4" borderRadius="md" boxShadow="lg" />
-            </Box>
-            </HStack>
-  
-          {/* Contiene el link del porfolio */}
-          <Box className="link-porfolio" textAlign="left" mt="10">
-            <Link href="https://github.com/ChrisChamizo" isExternal fontSize="lg" color="teal.300" textDecoration="none"
-          _hover={{ textDecoration: "none" }}>
-              Web Design / porfolio
-            </Link>
-            <Text fontSize="lg">
-              De Christian Chamizo.{' '}
-              <Link href="https://www.behance.net/" isExternal color="teal.300" textDecoration="none"
-          _hover={{ textDecoration: "none" }}>
-                Seguir
-              </Link>
-            </Text>
+      <Box 
+      mx="auto" 
+      maxW="1200px" 
+      px={{ base: "5", md: "10" }}>
+        <HStack
+        direction={{ base: "column", md: "row" }}
+        spacing="6" 
+        justify="center">
+          <Box
+           className="screenshot-portfolio"
+           >
+            <Image
+             src={imagentp3}
+            alt="screenshot de porfolio3"
+            borderRadius="md"
+            boxShadow="lg"
+            />
           </Box>
+          <Box
+           className="screenshot-portfolio"
+           >
+            <Image
+             src={imagentp4}
+              alt="screenshot de porfolio4"
+              borderRadius="md" 
+              boxShadow="lg" />
+          </Box>
+          <Box
+           className="screenshot-portfolio"
+           >
+            <Image
+             src={imagentpreact}
+              alt="screenshot de porfolio4"
+              borderRadius="md"
+              boxShadow="lg"
+            />
+          </Box>
+        </HStack>
+
+        <Box className="link-porfolio" textAlign={{ base: "center", md: "left" }} mt="10">
+          <Link
+            href="https://github.com/ChrisChamizo"
+            isExternal
+            fontSize="lg"
+            color="teal.300"
+            textDecoration="none"
+            _hover={{ textDecoration: "none" }}
+          >
+            Web Design / porfolio
+          </Link>
+          <Text fontSize="lg">
+            De Christian Chamizo.{' '}
+            <Link
+              href="https://www.behance.net/"
+              isExternal
+              color="teal.300"
+              textDecoration="none"
+              _hover={{ textDecoration: "none" }}
+            >
+              Seguir
+            </Link>
+          </Text>
         </Box>
       </Box>
-    );
-  };
-  
-  export { MiPortafolio };
+    </Box>
+  );
+};
+
+export { MiPortafolio };
